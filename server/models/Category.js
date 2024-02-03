54c8ae9371e32ddbd6991af24a59364fa3c7309f
Adam <adamsoliman800@gmail.com>
2024-02-03 22:58:12 +0200

    update Category.js

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  CategoryID: { type: String, required: true, unique: true },
+  CategoryID: { type: Number, required: true, unique: true },
   CategoryName: { type: String, required: true },
+}, {
+  timestamps: true,
+});
+
+categorySchema.pre('save', function (next) {
+  const doc = this;
+  if (!doc.isNew) {
+    return next();
+  }
+
+  // Auto-increment CategoryID starting from 1
+  mongoose.model('Category').countDocuments({}, (err, count) => {
+    if (err) {
+      return next(err);
+    }
+    doc.CategoryID = count + 1;
+    next();
+  });
 });
 
 const Category = mongoose.model('Category', categorySchema);
