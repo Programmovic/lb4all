39b39948d3d9433a2a823f7c1f566d8e9d9a8127
Adam <adamsoliman800@gmail.com>
2024-02-04 22:42:36 +0200

    update Brand.js

--- server/models/Brand.js
+++ server/models/Brand.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const brandSchema = new mongoose.Schema({
-  BrandID: { type: String, required: true, unique: true },
+  BrandID: { type: Number, required: true, unique: true },
   BrandName: { type: String, required: true },
   Description: { type: String },
   Logo: { type: String },
